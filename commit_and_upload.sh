0356fb129f586327524b92e7df2a3fa860374329
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-10-22 00:30:35 +0200

    upgrade commit_and_upload script

--- commit_and_upload.sh
+++ commit_and_upload.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 ftp_password=$1
-files_to_upload=`git ls-files --others --modified`
+
+curl --ftp-create-dirs -T blank_file -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu/blank_file
+if [ $? -ne 0 ]
+then
+	echo
+	echo
+	echo "Le password FTP indiqué est incorrect. Veuillez réessayer."
+	exit -1
+else
+	echo "password correct, on continue"
+fi
+
+cd mammusique.com
+files_to_upload=`git ls-files --others --modified --exclude-from=.gitignore`
 for file_to_upload in $files_to_upload
 do
-	git add $file_to_upload
-	curl --ftp-create-dirs -T $file_to_upload -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu/$file_to_upload
+	echo $file_to_upload
+	#git add $file_to_upload
+	#curl --ftp-create-dirs -T $file_to_upload -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu/$file_to_upload
 done
 
-git commit -m "save"
-git pull -r
+#git commit -m "save"
+#git pull -r
 git push
