64a73072ab6b12d56c6d062e7db53363a5a593fb
Mammusique <mammusique@gmail.com>
2016-01-30 00:41:25 +0100

    save

--- mammusique.com/accueil/menutexte.php
+++ mammusique.com/accueil/menutexte.php
@@ -1,1 +1,53 @@
-<title>accueil</title> <script language="JavaScript"><!--function MM_openBrWindow(theURL,winName,features) { //v2.0  window.open(theURL,winName,features);}//--></script><body leftmargin="25"><div align="left">   <p><font face="Arial, Helvetica, sans-serif" size="1"><font face="Arial, Helvetica, sans-serif" size="2"><font face="Arial, Helvetica, sans-serif" size="1"><img src="photo.accueil.jpg" width="213" height="312" align="left" hspace="10"><a href="javascript:;" onClick="MM_openBrWindow('../bio/viviane.html','bio','top=25, left=25, height=435, width=380')"><font size="2">Viviane     Arnoux</font></a> </font>et <a href="javascript:;" onClick="MM_openBrWindow('../bio/francois.html','bio','top=25, left=25, height=435, width=380')">Fran&ccedil;ois     Michaud</a><font face="Arial, Helvetica, sans-serif" size="1"> </font>collaborent     avec bonheur depuis 1992. De leur rencontre est n&eacute;e la n&eacute;cessit&eacute;     d&#146;&eacute;laborer un projet musical original pour accord&eacute;on, violon     (ou alto) et voix,en transgressant les habitudes et les r&ocirc;les instrumentaux.<br>    <br>    Un jazz se dessine alors, qui transfigure folklores et musiques du monde &agrave;     travers des compositions, des arrangements et des improvisations qui les racontent.     Vivifiante et nature, cette fiesta m&eacute;lodico-rythmique culott&eacute;e,     nourrie d&#146;explorations acoustiques, d&#146;envies de chanter et d&#146;int&eacute;grer     des sonorit&eacute;s nouvelles, convie au partage artistes complices et auditeurs     gourmands.<br>    <br>    Fondateurs de MAM, ces 2 musiciens &agrave; l&#146;art vagabond se produisent     souvent en duo, mais, friands de rencontres esth&eacute;tiques, ils multiplient     les aventures avec des comp&egrave;res d&#146;horizons &eacute;clectiques,     enregistrent 7 albums chez Buda Musique (dont le dernier, &quot;Paris, Village&quot;,     est une commande de l'&eacute;diteur Kosinus) en usant du duo jusqu&#146;au     quintet (avec violoncelle, percussions, batterie, invit&eacute;s&#133;). Leur     version de Mission Impossible (Lalo Schiffrin) a &eacute;t&eacute; reprise     dans 2 compilations et diffus&eacute;e sur France 2. Ils enregistrent 3 cds     avec des chanteurs japonais, participent &agrave; ceux de Mama Kaya (2), Thomas     Pitiot (2), G&eacute;rard Pitiot (3), Jean Piero, Alifair, Gilliane Kim, Thomasi     (2), fricotent avec le swing-musette rac&eacute; du virtuose accord&eacute;oniste     Fran&ccedil;ois Parisi (&laquo; Jazz in my Musette &raquo;),</font> <font size="2">recr&eacute;ent</font><font face="Arial, Helvetica, sans-serif" size="2">     Brassens sans guitare ni moustache avec la chanteuse Gu&eacute;na&euml;lle,     montent trois ambitieux projets pluridisciplinaires: &quot;Meddled Times&quot;     (avec Fab, musicien et ing&eacute;nieur du son new-yorkais), &quot;New Time&quot;     (avec Norbert Lucarain, &agrave; la human beat-box et aux claviers!) tous     deux avec machines et VJing, puis &quot;M&eacute;lodies Extras Ordinaires&quot;     (avec la com&eacute;dienne C&eacute;line Caussimon), conte musical pour grands     et petits, avec ombres chinoises et vid&eacute;o.</font></font><font face="Arial, Helvetica, sans-serif" size="1"><font face="Arial, Helvetica, sans-serif" size="2"><br>    <br>    Comme voyager se veut, pour eux, un art de vivre, ils n&#146;ont pas h&eacute;sit&eacute;     &agrave; se produire &agrave; l&#146;&eacute;tranger : Montr&eacute;al, Vancouver,     Halifax, Ouagadougou, Tripoli, St-Louis du S&eacute;n&eacute;gal, Tlemcen,     Skopje, Ljubljana, Innsbruck, Dresde, Bruges, Fribourg, Halmstad&#133; en     plus de red&eacute;couvrir inlassablement les 6 coins de France, gr&acirc;ce     notamment &agrave; 2 ann&eacute;es de tourn&eacute;es J.M.F. et 3 tourn&eacute;es     CCAS. Au menu cette ann&eacute;e : sortie d'un DVD &quot;Jazz in my Musette&quot;,     d'une compilation &quot;20 Ans&quot;, enregistrement de l'album &quot;New     Time&quot;, tourn&eacute;e au Canada.</font></font></p>  </div>
+<title>accueil</title> 
+<script language="JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+<body leftmargin="25">
+<div align="left"> 
+  <p><font face="Arial, Helvetica, sans-serif" size="1"><font face="Arial, Helvetica, sans-serif" size="2"><font face="Arial, Helvetica, sans-serif" size="1"><img src="photo.accueil.jpg" width="213" height="312" align="left" hspace="10"><a href="javascript:;" onClick="MM_openBrWindow('../bio/viviane.html','bio','top=25, left=25, height=435, width=380')"><font size="2">Viviane 
+    Arnoux</font></a> </font>et <a href="javascript:;" onClick="MM_openBrWindow('../bio/francois.html','bio','top=25, left=25, height=435, width=380')">Fran&ccedil;ois 
+    Michaud</a> collaborent 
+    avec bonheur depuis 1992. De leur rencontre est n&eacute;e la n&eacute;cessit&eacute; 
+    d&#146;&eacute;laborer un projet musical original pour accord&eacute;on, violon, alto et voix, en transgressant les habitudes et les r&ocirc;les instrumentaux.<br>
+    <br>
+    Un jazz se dessine alors, qui transfigure folklores et musiques du monde &agrave; 
+    travers des compositions, des arrangements et des improvisations qui les racontent. 
+    Vivifiante et nature, cette fiesta m&eacute;lodico-rythmique culott&eacute;e, 
+    nourrie d&#146;explorations acoustiques, d&#146;envies de chanter et d&#146;int&eacute;grer 
+    des sonorit&eacute;s nouvelles, convie au partage artistes complices et auditeurs 
+    gourmands.<br>
+    <br>
+    Fondateurs de MAM, ces 2 musiciens &agrave; l&#146;art vagabond se produisent 
+    souvent en duo, mais, friands de rencontres esth&eacute;tiques, ils multiplient 
+    les aventures avec des comp&egrave;res d&#146;horizons &eacute;clectiques, 
+    enregistrent 8 albums chez Buda Musique (dont l'avant-dernier, &quot;Paris Village&quot;, 
+    est une commande de l'&eacute;diteur Kosinus), usant du duo jusqu&#146;au 
+    quintet (avec violoncelle, percussions, batterie, invit&eacute;s&#133;). Leur 
+    version de Mission Impossible (Lalo Schiffrin) a &eacute;t&eacute; reprise 
+    dans 2 compilations et diffus&eacute;e sur France 2. Ils enregistrent 3 cds 
+    avec des chanteurs japonais, participent &agrave; ceux de Mama Kaya (2), Thomas 
+    Pitiot (2), G&eacute;rard Pitiot (3), Jean Piero, Alifair, Gilliane Kim, Thomasi 
+    (2), fricotent avec le swing-musette rac&eacute; du virtuose accord&eacute;oniste 
+    Fran&ccedil;ois Parisi (&quot;Jazz in my Musette&quot;),</font> <font size="2">recr&eacute;ent</font><font face="Arial, Helvetica, sans-serif" size="2"> 
+    Brassens sans guitare ni moustache avec la chanteuse Gu&eacute;na&euml;lle, 
+    montent trois ambitieux projets pluridisciplinaires: &quot;Meddled Times&quot; 
+    (avec Fab, musicien et ing&eacute;nieur du son new-yorkais), &quot;New Time&quot; 
+    (avec Norbert &quot;Touski&quot; Lucarain, &agrave; la human beat-box et aux claviers!) tous 
+    deux avec machines et VJing, puis &quot;M&eacute;lodies Extras Ordinaires&quot; 
+    (avec la com&eacute;dienne C&eacute;line Caussimon), conte musical pour grands 
+    et petits, avec ombres chinoises et vid&eacute;o.</font></font><font face="Arial, Helvetica, sans-serif" size="1"><font face="Arial, Helvetica, sans-serif" size="2"> En 2015, ils créent un spectacle et enregistrent un album, &quot;Human Swing Box&quot;, toujours avec Touski.<br>
+    <br>
+    Comme voyager se veut, pour eux, un art de vivre, ils n&#146;ont pas h&eacute;sit&eacute; 
+    &agrave; se produire &agrave; l&#146;&eacute;tranger : Montr&eacute;al, Vancouver, 
+    Halifax, Ouagadougou, Tripoli, St-Louis du S&eacute;n&eacute;gal, Tlemcen, 
+    Skopje, Ljubljana, Innsbruck, Dresde, Bruges, Fribourg, Halmstad, Saint-Domingue, CapHaïtien, Caracas, Iquitos, Dubaï&#133; en 
+    plus de red&eacute;couvrir inlassablement les 6 coins de France, gr&acirc;ce 
+    notamment &agrave; 2 ann&eacute;es de tourn&eacute;es J.M.F. et 3 tourn&eacute;es 
+    CCAS. Au menu prochainement : sortie d'un DVD &quot;Jazz in my Musette&quot;, 
+    d'une compilation &quot;25 Ans&quot;, enregistrement de l'album &quot;New 
+    Time&quot;, tourn&eacute;e aux Émirats, création de l'épisode 2 de &quot;Mélodies Extras Ordinaires&quot;.</font></font></p>
+  </div>
