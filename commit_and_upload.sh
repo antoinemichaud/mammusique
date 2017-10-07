6ed77a738572a52f646680411811d3b8b4e00664
Antoine MICHAUD <antoinemichaud34@gmail.com>
2017-10-07 03:57:15 +0200

    be able to add comment

--- commit_and_upload.sh
+++ commit_and_upload.sh
@@ -2,8 +2,8 @@
 ftp_password=$1
 commit_message="save"
 
-if [[ $* == 2 ]]; then
-	commit_message=$2
+if [[ $# == 2 ]]; then
+	commit_message="$2"
 fi
 
 curl --ftp-create-dirs -T blank_file -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu/blank_file
@@ -26,6 +26,6 @@ do
 	curl --ftp-create-dirs -T $file_to_upload -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu/$file_to_upload
 done
 
-git commit -m $commit_message
+git commit -m "$commit_message"
 git pull -r
 git push
