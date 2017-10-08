631639c6f6df499ac0fe853496857be45bac397a
Antoine MICHAUD <antoinemichaud34@gmail.com>
2017-10-08 19:38:35 +0200

    justify english text

--- mammusique.com/english/accueil.php
+++ mammusique.com/english/accueil.php
@@ -2,45 +2,48 @@
 include_once "header.inc"
 ?>
 
-<div align="left"> <img src="photo.accueil.jpg" width="220" height="313" align="left" hspace="10"><a href="javascript:;" onClick="window.open('bio/viviane.html','bio','top=25, left=25, height=435, width=380')">Viviane
-        Arnoux</a> and <a href="javascript:;" onClick="window.open('bio/francois.html','bio','top=25, left=25, height=435, width=380')">Fran&ccedil;ois
-        Michaud</a> have been playing happily together since 1992. The two of
-    them worked out an original musical project for accordion, violin, viola
-    and vocals by altering the usual role of each musical instrument.<br>
-    <br>
-    A new jazz form was born that transmuted folklore and ethnic world music into
-    original compositions, arrangements and improvisations. Stimulating and natural,
-    this provocative fiesta of rhythms and melodies feeds on acoustic explorations
-    and the urge to sing and bring together new sounds. Musicians are welcome
-    to join as guests while listeners are invited
-    to the feast.<br>
-    <br>
-    These two restless musicians, the founders of MAM, often play in duos; yet,
-    always anxious to make novel musical encounters they delight in performing with
-    fellow musicians of different backgrounds. They recorded 8 CD&#8217;s with Buda
-    Musique (the second to last, &quot;Paris Village&quot; was an order from the publisher
-    Kosinus) in various formations from duet to quintet (guest cello, percussion
-    and drums). Their version of &#8220;Mission Impossible&#8221; (Lalo Schiffrin)
-    was twice compiled and played on tv (France 2). They recorded 3 CD&#8217;s with
-    Japanese singers, participated in those by Mama Kaya (2), Thomas Pitiot (2),
-    G&eacute;rard Pitiot (3), Jean Piero, Alifair, Gilliane Kim, Thomasi (2). They
-    &#8220;made a pass&#8221; at the racy swing musette of accordionist virtuoso
-    Fran&ccedil;ois Parisi (&quot;Jazz in my Musette&quot;). They
-    put on a Brassens performance minus guitar and moustache but with the female
-    singer Gu&eacute;na&euml;lle and are currently working on 4 ambitious multifaced
-    projects: &quot;Meddled Times&quot; with Fab, a famous French-Newyorker musician and
-    sound engineer, &quot;New Time&quot; with the fantastic showman Norbert &quot;Touski&quot; Lucarain, playing human beatbox
-    and keyboards together!
-    (these 2 shows featuring VJing and machines) then, &quot;M&eacute;lodies Extras
-    Ordinaires&quot; (featuring
-    the actress C&eacute;line Caussimon), and &quot;La M&eacute;lodie des Choses&quot; (featuring Pauline Borras, de La Compagnie Pr&eacute;fabriqu&eacute;e), two musical tales with theater, dance, video and chineese shadows. In 2015, they created &quot;Human Swing Box&quot; and recorded the album, still playing with Touski.<br>
-    <br>
-    Travelling is for them an &#8220;art de vivre&#8221;, no wonder they have often
-    performed abroad: Montreal, Vancouver, Halifax, Ouagadougou, Tripoli, St Louis
-    in Senegal, Tlemcen, Skopje, Ljubjana, Innsbruck, Dresden, Bruges, Freiburg
-    (CH), Halmstad,  Santo-Domingo, Cap-Haitien, Caracas, Iquitos, Dubai&#8230; in addition to crisscrossing hexagonal France. This season&#8217;s
-    menu : a DVD for &quot;Jazz in my Musette&quot;, a &quot;25 Years&quot; compilation album, and so, celebration concerts next fall.</div>
-
+<div>
+    <p class="general-style" style="text-align: justify">
+        <img src="photo.accueil.jpg" width="220" height="313" align="left" hspace="10"><a href="javascript:;" onClick="window.open('bio/viviane.html','bio','top=25, left=25, height=435, width=380')">Viviane
+            Arnoux</a> and <a href="javascript:;" onClick="window.open('bio/francois.html','bio','top=25, left=25, height=435, width=380')">Fran&ccedil;ois
+            Michaud</a> have been playing happily together since 1992. The two of
+        them worked out an original musical project for accordion, violin, viola
+        and vocals by altering the usual role of each musical instrument.<br>
+        <br>
+        A new jazz form was born that transmuted folklore and ethnic world music into
+        original compositions, arrangements and improvisations. Stimulating and natural,
+        this provocative fiesta of rhythms and melodies feeds on acoustic explorations
+        and the urge to sing and bring together new sounds. Musicians are welcome
+        to join as guests while listeners are invited
+        to the feast.<br>
+        <br>
+        These two restless musicians, the founders of MAM, often play in duos; yet,
+        always anxious to make novel musical encounters they delight in performing with
+        fellow musicians of different backgrounds. They recorded 8 CD&#8217;s with Buda
+        Musique (the second to last, &quot;Paris Village&quot; was an order from the publisher
+        Kosinus) in various formations from duet to quintet (guest cello, percussion
+        and drums). Their version of &#8220;Mission Impossible&#8221; (Lalo Schiffrin)
+        was twice compiled and played on tv (France 2). They recorded 3 CD&#8217;s with
+        Japanese singers, participated in those by Mama Kaya (2), Thomas Pitiot (2),
+        G&eacute;rard Pitiot (3), Jean Piero, Alifair, Gilliane Kim, Thomasi (2). They
+        &#8220;made a pass&#8221; at the racy swing musette of accordionist virtuoso
+        Fran&ccedil;ois Parisi (&quot;Jazz in my Musette&quot;). They
+        put on a Brassens performance minus guitar and moustache but with the female
+        singer Gu&eacute;na&euml;lle and are currently working on 4 ambitious multifaced
+        projects: &quot;Meddled Times&quot; with Fab, a famous French-Newyorker musician and
+        sound engineer, &quot;New Time&quot; with the fantastic showman Norbert &quot;Touski&quot; Lucarain, playing human beatbox
+        and keyboards together!
+        (these 2 shows featuring VJing and machines) then, &quot;M&eacute;lodies Extras
+        Ordinaires&quot; (featuring
+        the actress C&eacute;line Caussimon), and &quot;La M&eacute;lodie des Choses&quot; (featuring Pauline Borras, de La Compagnie Pr&eacute;fabriqu&eacute;e), two musical tales with theater, dance, video and chineese shadows. In 2015, they created &quot;Human Swing Box&quot; and recorded the album, still playing with Touski.<br>
+        <br>
+        Travelling is for them an &#8220;art de vivre&#8221;, no wonder they have often
+        performed abroad: Montreal, Vancouver, Halifax, Ouagadougou, Tripoli, St Louis
+        in Senegal, Tlemcen, Skopje, Ljubjana, Innsbruck, Dresden, Bruges, Freiburg
+        (CH), Halmstad,  Santo-Domingo, Cap-Haitien, Caracas, Iquitos, Dubai&#8230; in addition to crisscrossing hexagonal France. This season&#8217;s
+        menu : a DVD for &quot;Jazz in my Musette&quot;, a &quot;25 Years&quot; compilation album, and so, celebration concerts next fall.
+    </p>
+</div>
 <?php
 include_once "footer.inc"
 ?>
